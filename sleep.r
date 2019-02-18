415ab1549f80eabbbf01ee327d4356375cd833df
Iain Crossingham <irc91914@gmail.com>
2019-02-18 18:09:17 +0000

    ggplot2 noodles

--- sleep.r
+++ sleep.r
@@ -24,4 +24,23 @@ oxi1 <- oxi %>%
             Pulse = as.integer(T2),
             Exception = Exception)
 
+# Single variable plot
 g1 <- ggplot(oxi1, aes(Timepoint, SpO2)) + geom_step(colour = "red")
+g2 <- ggplot(oxi1, aes(Timepoint, Pulse)) + geom_step(colour = "blue")
+
+# Plot both heart rate and oximetry
+plot_colours <- c("SpO2" = "red", "Pulse" = "blue")
+g3 <- ggplot(oxi1, aes(x = Timepoint)) + 
+  geom_step(aes(y = SpO2, colour = plot_colours["SpO2"])) +
+  geom_step(aes(y = Pulse, colour = plot_colours["Pulse"])) +
+  labs(y = "Oxygen saturations",
+       x = "Time",
+       colour = "Signal") +
+  scale_y_continuous(sec.axis = sec_axis(~., name = "Heart rate (bpm)"))
+
+# Plot just one hour
+onehour <- interval(dmy_hm("10.2.2019_21:59"), dmy_hm("10.2.2019_22:59"))
+# Alternate form for interval()
+hourtwo <- dmy_hm("10.2.2019_22:59") %--% dmy_hm("10.2.2019_23:59")
+g4 <- ggplot(filter(oxi1, Timepoint %within% onehour), aes(Timepoint, SpO2)) +
+  geom_step(colour = "red")
